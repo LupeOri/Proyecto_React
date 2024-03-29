911b588bd94bcdb5842f81f1ce26e2c08f0f1f9e
lupeori <lupe.oriol@gmail.com>
2024-03-29 02:45:36 +0100

    modif Searcher y ListaMovies

--- src/assets/components/layout/ListaMovies.jsx
+++ src/assets/components/layout/ListaMovies.jsx
@@ -2,33 +2,34 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Movies from "./Movies";
 
-export const getMovies = async (key) => {
+function ListaMovies() {
   const API_URL = "https://api.themoviedb.org/3";
   const API_KEY = "bbaa7afeb202fbf5a15e8b9f2c114a45";
+  const IMAGE_PAT = "https://image.tmdb.org/t/p/original";
   const URL_IMAGE = "https://image.tmdb.org/t/p/original";
 
-  const type = key ? "search" : "discover";
-  const response = await axios.get(`${API_URL}/${type}/movie`, {
-    params: {
-      api_key: API_KEY,
-      query: key,
-    },
-  });
-  const dataMovies = response.data.results.map((movie) => ({
-    id: movie.id,
-    img: `${URL_IMAGE + movie.poster_path}`,
-    title: movie.title,
-  }));
-  return dataMovies;
-};
-
-function ListaMovies() {
   const [movies, setMovies] = useState([]);
+  const [key, setkey] = useState("");
+  const [movie, setMovie] = useState({ title: "Loading movies" });
+
+  const getMovies = async (key) => {
+    const type = key ? "search" : "discover";
+    const response = await axios.get(`${API_URL}/${type}/movie`, {
+      params: {
+        api_key: API_KEY,
+        query: key,
+      },
+    });
+    const dataMovies = response.data.results.map((movie) => ({
+      id: movie.id,
+      img: `${URL_IMAGE + movie.poster_path}`,
+      title: movie.title,
+    }));
+    setMovies(dataMovies);
+  };
 
   useEffect(() => {
-    getMovies().then((dataMovies) => {
-      setMovies(dataMovies);
-    });
+    getMovies();
   }, []);
 
   return (
--- src/assets/components/layout/Searcher.jsx
+++ src/assets/components/layout/Searcher.jsx
@@ -1,13 +1,37 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { getMovies } from "./ListaMovies";
 
 function Searcher() {
-  const [key, setKey] = useState("");
+  const API_URL = "https://api.themoviedb.org/3";
+  const API_KEY = "bbaa7afeb202fbf5a15e8b9f2c114a45";
+  const IMAGE_PAT = "https://image.tmdb.org/t/p/original";
+  const URL_IMAGE = "https://image.tmdb.org/t/p/original";
+
+  const [movies, setMovies] = useState([]);
+  const [key, setkey] = useState("");
+  const [movie, setMovie] = useState({ title: "Loading movies" });
+
+  const getMovies = async (key) => {
+    const type = key ? "search" : "discover";
+    const response = await axios.get(`${API_URL}/${type}/search`, {
+      params: {
+        api_key: API_KEY,
+        query: key,
+      },
+    });
+    const dataMovies = response.data.results.map((movie) => ({
+      id: movie.id,
+      img: `${URL_IMAGE + movie.poster_path}`,
+      title: movie.title,
+    }));
+    setMovies(dataMovies);
+  };
 
   const searchMovies = (e) => {
     e.preventDefault();
-    getMovies(key);
+    if (key.trim() !== "") {
+      getMovies(key);
+    }
   };
 
   return (
@@ -16,9 +40,9 @@ function Searcher() {
         <form className="searcher-form" onSubmit={searchMovies}>
           <input
             type="text"
-            placeholder="Escriba el nombre de la película..."
+            placeholder="Escriba el nombre de la pelicula..."
             value={key}
-            onChange={(e) => setKey(e.target.value)}
+            onChange={(e) => setkey(e.target.value)}
           />
           <button type="submit" className="btn-search">
             Buscar
